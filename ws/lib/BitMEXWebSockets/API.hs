615143b6228eabc2d9abdd335463a43c2868b9b6
Lucsanszky <dan.lucsanszky@gmail.com>
2018-05-04 03:08:45 +0100

    Add an example to the client, using the WS types

--- ws/lib/BitMEXWebSockets/API.hs
+++ ws/lib/BitMEXWebSockets/API.hs
@@ -1,23 +1,21 @@
-{-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE NoImplicitPrelude     #-}
-{-# LANGUAGE OverloadedStrings     #-}
-
 module BitMEXWebSockets.API
     ( app
     ) where
 
-import           Control.Concurrent  (forkIO)
-import           Control.Monad       (forever, unless)
-import           Control.Monad.Trans (liftIO)
-import           Data.Text           (Text, null)
-import           Data.Text.IO        (getLine, putStrLn)
+import           BitMEXWebSockets.Type
+import           Control.Concurrent    (forkIO)
+import           Control.Monad         (forever, unless)
+import           Control.Monad.Trans   (liftIO)
+import           Data.Aeson            (encode)
+import           Data.Text             (Text, null)
+import           Data.Text.IO          (getLine, putStrLn)
 import           Network.WebSockets
     ( ClientApp
     , receiveData
     , sendClose
     , sendTextData
     )
-import           Prelude             (($), (>>), (>>=))
+import           Prelude               (($), (>>), (>>=))
 
 app :: ClientApp ()
 app conn = do
@@ -26,6 +24,7 @@ app conn = do
         forever $ do
             msg <- receiveData conn
             liftIO $ putStrLn msg
+    sendTextData conn $ encode $ Message {op = Subscribe, args = [Chat]}
     loop
     sendClose conn ("Connection closed" :: Text)
   where
